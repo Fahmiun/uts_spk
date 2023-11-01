5d6dac1360f172efc9314998310f34fd37270edc
Fahmiun <149554183+Fahmiun@users.noreply.github.com>
2023-11-01 10:50:34 +0700

    Add files via upload

---
+++ spk/harga_hp_202311011036.sql
@@ -0,0 +1,11 @@
+INSERT INTO uts_spk.harga_hp (hp,harga,ram,kapasitas_baterai,processor,penyimpanan_internal) VALUES
+	 ('Xiaomi 13T','Rp6.499.000',12,'5000mAh','Dimensity 8200 Ultra','256 GB'),
+	 ('vivo Y17s','Rp1.799.000',6,'5000mAh','Helio G85 (12nm)','128 GB'),
+	 ('vivo V29 5G','Rp6.200.000',12,'4600mAh','Snapdragon 778G 5G (6 nm)','512 GB'),
+	 ('realme 11','Rp3.599.000',8,'5000mAh','Helio G99','256 GB'),
+	 ('Infinix GT 10 Pro','Rp3.330.000',8,'5000mAh','Dimensity 8050 (6 nm)','256 GB'),
+	 ('realme C51','Rp1.709.000',4,'5000mAh','Tiger T612 (12 nm)','128 GB'),
+	 ('Redmi 12','Rp1.900.000',8,'5000mAh','Helio G88 (12nm)','256 GB'),
+	 ('vivo Y27 4G','Rp1.999.000',6,'5000mAh','MT6769 Helio G85','128 GB'),
+	 ('vivo Y27 5G','Rp2.450.000',6,'5000mAh','Dimensity 6020','128 GB'),
+	 ('Samsung Galaxy Z Flip5','Rp15.000.000',8,'3700mAh','Snapdragon 8 Gen 2','512 GB');
